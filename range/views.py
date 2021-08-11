d99af79f3be6200fcb5ff3f8f579fb79ffd59f18
Farhan Basheer <farhanfb6@outlook.com>
2021-08-11 19:33:24 +0500

    Calculation Through Javascript is Working.

--- range/views.py
+++ range/views.py
@@ -2,15 +2,6 @@ from django.shortcuts import render
 from range.models import price_range
 
 
-# def showobj(request):
-#    displayvalues = price_range.objects.all()
-#    return render(request, 'range/home.html', {"range":displayvalues})
-
-# def displayvalue(request, id):
-#    displayprice = price_range.objects.get(id=id)
-#    context = {'displayprice':displayprice}
-#    return render(request, 'range/price_range.html', context)
-
 def showobj(request):
     selected_range = None
     range_price = price_range.objects.all()
